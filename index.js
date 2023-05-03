fae8d27d6aa8c20b3facf110ca5f5ba44db1942a
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-05-03 16:14:13 +0600

    chef recipes data sending client side

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 5000;
-const cors=require('cors')
-const chefdata = require('./chefdata/data.json');
-const chefrecipes = require('./chefdata/chefrecipes.json');
+const cors = require("cors");
+const chefdata = require("./chefdata/data.json");
+const chefrecipes = require("./chefdata/chefrecipes.json");
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('tasty cookes compass is running')
-})
+app.get("/", (req, res) => {
+  res.send("tasty cookes compass is running");
+});
 
-app.get('/chefdata', (req, res) => {
+app.get("/chefdata", (req, res) => {
   res.send(chefdata);
-})
+});
 
-app.get('/chefrecipes', (req, res) => {
-  res.send(chefrecipes)
-})
+app.get("/chefrecipes/", (req, res) => {
+  res.send(chefrecipes);
+});
+
+app.get("/chefdata/:id", (req, res) => {
+  const id = req.params.id;
+  const selectId = chefdata.find((recipes) => recipes.id === id);
+  res.send(selectId);
+});
 
 app.listen(port, () => {
   console.log(`tasty cookes compass is running on port: ${port}`);
-})
+});
