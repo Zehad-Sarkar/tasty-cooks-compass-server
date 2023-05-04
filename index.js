a8a208a531d840c05d047f204031b711faa5f48c
Zehad-Sarkar <zehadsarkar22@gmail.com>
2023-05-04 18:33:35 +0600

    redeploy and updated code

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+const categories = require("./chefdata/list.json");
 const chefrecipes = require("./chefdata/chefrecipes.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("tasty cookes compass is running");
 });
+
+app.get("/list", (req, res) => {
+  res.send(categories);
+})
+app.get("/list/:id", (req, res) => {
+   const id = req.params.id;
+   const selectId = categories.filter((c) => c.id === id);
+   res.send(selectId);
+})
 
 app.get("/chefrecipes", (req, res) => {
   res.send(chefrecipes);
